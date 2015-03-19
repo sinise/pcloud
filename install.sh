c75ef1baa71a9d5c6b60596c03e005e5bf068bc6
sinise <sebastian@lapela.dk>
2015-03-19 12:19:31 +0000

    minor

--- install.sh
+++ install.sh
@@ -5,39 +5,42 @@
 #set overclock maybe to 900 on rasp b+ rasp2 should maybe be set a litle higher
 #set defoult login to desktop
 
-#to make outologin to shell open /etc/inittab and replace line
-#   1:2345:respawn:/sbin/getty --noclear 38400 tty1
-#with
-#   1:2345:respawn:/bin/login -f pi tty1 </dev/tty1 2>&1
 
-#apt-get -y update
-#apt-get -y upgrade
-#apt-get -y dist-upgrade
-#apt-get -y install rpi-update
-#rpi-update
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install rpi-update
+rpi-update
 
-#apt-get -y install chromium
-#apt-get -y install unclutter #remove mouse
-#apt-get -y install ttf-mscorefonts-installer #install fonts
-#apt-get -y install chromium-browser
-#apt-get -y install x11-xserver-utils
+apt-get -y install chromium
+apt-get -y install unclutter #remove mouse
+apt-get -y install ttf-mscorefonts-installer #install fonts
+apt-get -y install chromium-browser
+apt-get -y install x11-xserver-utils
 
 #Disable power management for the wifi
-#echo "# Disable power management" >> /etc/modprobe.d/8192cu.conf
-#echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=0" >> /etc/modprobe.d/8192cu.conf
+echo "# Disable power management" >> /etc/modprobe.d/8192cu.conf
+echo "options 8192cu rtw_power_mgnt=0 rtw_enusbss=0" >> /etc/modprobe.d/8192cu.conf
 
 #copy rasp settings
-#cp config.txt /boot/config.txt
+cp config.txt /boot/config.txt
 chmod 666 /boot/config.txt
 
 #make shure the screen does not dim
 cp config /etc/kbd/config
 
+#disabale login 
+#to make outologin to shell open /etc/inittab and replace line
+#   1:2345:respawn:/sbin/getty --noclear 38400 tty1
+#with
+#   1:2345:respawn:/bin/login -f pi tty1 </dev/tty1 2>&1
+cp inittab /etc/inittab
+
 #Register chromium as a service
 echo "add chromium as service"
-#cp chromium.sh /etc/init.d/
+cp chromium.sh /etc/init.d/
 chmod 777 /etc/init.d/chromium.sh
-#update-rc.d filename defaults
+update-rc.d filename defaults
 echo "done"
 
 #setup cron job
