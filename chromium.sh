e6e5f97e82a5ebde1bb9f44a666e29f9e606b2ea
sebastian <sebastian@momentos.dk>
2015-03-19 13:37:55 +0000

    fsfds

--- chromium.sh
+++ chromium.sh
@@ -13,7 +13,6 @@ dir="/usr/bin"
 user="pi"
 url="http://www.krak.dk"
 cmd="xinit /usr/bin/chromium $url --kiosk --disable-translate --user-agent='Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36' --incognito --disable-java"
-echo $cmd >> home/pi/cmdtocrome
 
 name=`basename $0`
 pid_file="/var/run/$name.pid"
